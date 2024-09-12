515d0c6d5fe810c5bbfe66a681a926fef3b75295
atthharvva <atharva2003.swami@gmail.com>
2024-09-12 17:53:18 +0530

    openCV

---
+++ openCV/build/intermediates/cxx/RelWithDebInfo/4o2am6t4/logs/x86/configure_command.bat
@@ -0,0 +1,19 @@
+@echo off
+"C:\\Users\\athar\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\cmake.exe" ^
+  "-HC:\\Users\\athar\\AndroidStudioProjects\\Form-correction-ML-based-app\\openCV\\libcxx_helper" ^
+  "-DCMAKE_SYSTEM_NAME=Android" ^
+  "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON" ^
+  "-DCMAKE_SYSTEM_VERSION=24" ^
+  "-DANDROID_PLATFORM=android-24" ^
+  "-DANDROID_ABI=x86" ^
+  "-DCMAKE_ANDROID_ARCH_ABI=x86" ^
+  "-DANDROID_NDK=C:\\Users\\athar\\AppData\\Local\\Android\\Sdk\\ndk\\26.1.10909125" ^
+  "-DCMAKE_ANDROID_NDK=C:\\Users\\athar\\AppData\\Local\\Android\\Sdk\\ndk\\26.1.10909125" ^
+  "-DCMAKE_TOOLCHAIN_FILE=C:\\Users\\athar\\AppData\\Local\\Android\\Sdk\\ndk\\26.1.10909125\\build\\cmake\\android.toolchain.cmake" ^
+  "-DCMAKE_MAKE_PROGRAM=C:\\Users\\athar\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=C:\\Users\\athar\\AndroidStudioProjects\\Form-correction-ML-based-app\\openCV\\build\\intermediates\\cxx\\RelWithDebInfo\\4o2am6t4\\obj\\x86" ^
+  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=C:\\Users\\athar\\AndroidStudioProjects\\Form-correction-ML-based-app\\openCV\\build\\intermediates\\cxx\\RelWithDebInfo\\4o2am6t4\\obj\\x86" ^
+  "-DCMAKE_BUILD_TYPE=RelWithDebInfo" ^
+  "-BC:\\Users\\athar\\AndroidStudioProjects\\Form-correction-ML-based-app\\openCV\\.cxx\\RelWithDebInfo\\4o2am6t4\\x86" ^
+  -GNinja ^
+  "-DANDROID_STL=c++_shared"
